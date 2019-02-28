aa1e5607c6ef598029a90740be05d821c2114638
Mannuel Ferreira <mannuel@themwebs.me>
2019-02-28 21:29:42 +0200

    added cors lib, gave my routes new meanfull names for the give collections

--- api/app.js
+++ api/app.js
@@ -1,17 +1,34 @@
 'use strict'
 
-var express = require('express')
-var path = require('path')
-var cookieParser = require('cookie-parser')
-var logger = require('morgan')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const cors = require('cors');
+const consola = require('consola')
 
-var indexRouter = require('./routes/index')
-// var usersRouter = require('./routes/users')
-// var citiesRouter = require('./routes/cities')
-var uploadRouter = require('./routes/upload')
+const indexRouter = require('./routes/index')
+const usersRouter = require('./routes/user/attributes')
+const citiesRouter = require('./routes/locations/cities')
+const uploadRouter = require('./routes/upload/upload')
 
-var app = express()
+const app = express()
 
+const whiteList = [
+  'http://localhost:3001',
+  'http://localhost:9000'
+]
+let corsOptions = {
+  origin: (origin, callback) => {
+    if (whiteList.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by cors'))
+    }
+  }
+}
+
+app.use(cors(corsOptions))
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -19,8 +36,8 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-// app.use('/users', usersRouter)
-// app.use('/cities', citiesRouter)
-app.use('/api/images', uploadRouter)
+app.use('/user/attributes', usersRouter)
+app.use('/locations/cities', citiesRouter)
+app.use('/upload', uploadRouter)
 
 module.exports = app
