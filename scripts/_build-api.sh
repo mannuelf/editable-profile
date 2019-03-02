84932439c5e844b20976bf1e078ae65cfbead1ea
Mannuel Ferreira <mannuel@themwebs.me>
2019-03-03 01:52:56 +0200

    dont need these here anymore, better in respective folder

--- scripts/_build-api.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -efu
-set -o pipefail
-
-cd ./api/ && docker build -t spark-api .
--- scripts/_build-client.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -efu
-set -o pipefail
-
-cd ./client/ && docker build -t spark-client .
